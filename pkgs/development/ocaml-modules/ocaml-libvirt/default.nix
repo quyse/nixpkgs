0e0f22c9d61eb8878699fd43a543a114e7c37e5f
Quyse Lert <quyse0@gmail.com>
2020-09-20 14:28:16 +0300

    ocaml-libvirt: set source to gitlab

--- pkgs/development/ocaml-modules/ocaml-libvirt/default.nix
+++ pkgs/development/ocaml-modules/ocaml-libvirt/default.nix
@@ -1,12 +1,13 @@
-{ stdenv, fetchgit, libvirt, autoconf, ocaml, findlib }:
+{ stdenv, fetchFromGitLab, libvirt, autoconf, ocaml, findlib }:
 
 stdenv.mkDerivation rec {
   pname = "ocaml-libvirt";
   rev = "bab7f84ade84ceaddb08b6948792d49b3d04b897";
   version = "0.6.1.4.2017-11-08-unstable"; # libguestfs-1.34+ needs ocaml-libvirt newer than the latest release 0.6.1.4
 
-  src = fetchgit {
-    url = "git://git.annexia.org/git/ocaml-libvirt.git";
+  src = fetchFromGitLab {
+    owner = "libvirt";
+    repo = "libvirt-ocaml";
     rev = rev;
     sha256 = "0vxgx1n58fp4qmly6i5zxiacr7303127d6j78a295xin1p3a8xcw";
   };
